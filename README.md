1fb10f4622177bba7ee97f4b97c168853cf8030f
Shoichi Kaji <skaji@cpan.org>
2016-05-14 17:06:47 +0900

    more eg

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 import Perl6 react syntax to Perl5
 
-```perl5
+```perl
 #!/usr/bin/env perl
 use 5.24.0;
 use experimental 'signatures';
@@ -15,6 +15,12 @@ react {
         my $user = $slack->find_user_name($message->{user});
         my $channel = $message->{channel};
         $slack->send_message($channel => "hello, $user!");
+    };
+    whenever $slack->subscribe("reaction_added") => sub ($event) {
+        my $reaction = $event->{reaction};
+        my $user = $slack->find_user_name($event->{user});
+        my $channel = $event->{item}{channel};
+        $slack->send_message($channel => "$user added $reaction!!");
     };
 };
 ```
--- app.pl
+++ app.pl
@@ -8,9 +8,15 @@ my $slack = Mojo::SlackRTM->new(token => $ENV{SLACK_TOKEN});
 $slack->connect;
 
 react {
-    whenever $slack->subscribe("message") => sub ($message) {
-        my $user = $slack->find_user_name($message->{user});
-        my $channel = $message->{channel};
+    whenever $slack->subscribe("message") => sub ($event) {
+        my $user = $slack->find_user_name($event->{user});
+        my $channel = $event->{channel};
         $slack->send_message($channel => "hello, $user!");
+    };
+    whenever $slack->subscribe("reaction_added") => sub ($event) {
+        my $reaction = $event->{reaction};
+        my $user = $slack->find_user_name($event->{user});
+        my $channel = $event->{item}{channel};
+        $slack->send_message($channel => "$user added $reaction!!");
     };
 };
